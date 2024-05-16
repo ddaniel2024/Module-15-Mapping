524ee5bd6591fb7336f11ffa2e614963828cdcf1
ddaniel2024 <daniel97daniel@gmail.com>
2024-05-16 20:37:57 +0100

    data imported with d3

---
+++ static/js/logic.js
@@ -0,0 +1,15 @@
+let myMap = L.map("map", {
+    center: [0,0],
+    zoom : 2
+  });
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(url).then(function(data) {console.log(data)
+
+    console.log(data.features[0].geometry.coordinates);
+});
